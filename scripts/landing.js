24fef9bcbd81a8993f1f433c01e1a220e486416d
Erin Johnson <erinjohnson21@gmail.com>
2016-08-02 20:38:11 -0500

    Checkpoint 23 Events:completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-         var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
 
          var revealPoints = function(index) {
            points[index].style.opacity = 1;
@@ -15,32 +15,12 @@ var animatePoints = function() {
 
 };
 
-
-
-         /* var revealFirstPoint = function() {
-             points[0].style.opacity = 1;
-             points[0].style.transform = "scaleX(1) translateY(0)";
-             points[0].style.msTransform = "scaleX(1) translateY(0)";
-             points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-         };
-
-         var revealSecondPoint = function() {
-             points[1].style.opacity = 1;
-             points[1].style.transform = "scaleX(1) translateY(0)";
-             points[1].style.msTransform = "scaleX(1) translateY(0)";
-             points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-         };
-
-         var revealThirdPoint = function() {
-             points[2].style.opacity = 1;
-             points[2].style.transform = "scaleX(1) translateY(0)";
-             points[2].style.msTransform = "scaleX(1) translateY(0)";
-             points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-         };
-
-         revealFirstPoint();
-         revealSecondPoint();
-         revealThirdPoint();
-
-
-     };*/
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+           animatePoints(pointsArray);
+       }
+  });
+}
