e9833c1f1d33887bf80392e52e1015e46627846c
Erin Johnson <erinjohnson21@gmail.com>
2016-08-23 20:04:17 -0500

    Checkpoint 30 jQuery:completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,11 +1,12 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
-   + '      <a class="album-name" href="/album.html"> The Colors </a>'
+   + '      <a class="album-name" href="album.html"> The Colors </a>'
    + '      <br/>'
-   + '      <a href="/album.html"> Pablo Picasso </a>'
+   + '      <a href="album.html"> Pablo Picasso </a>'
    + '      <br/>'
    + '      X songs'
    + '      <br/>'
@@ -13,15 +14,21 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
 
+   return $(template);
+};
 
-   window.onload = function() {
+
+   $(window).load(function() {
      // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
+     //var collectionContainer = document.getElementsByClassName('album-covers')[0];
+     var $collectionContainer = $('.album-covers');
+     //collectionContainer.innerHTML = '';
+     $collectionContainer.empty();
 
      // #3
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         //collectionContainer.innerHTML += collectionItemTemplate;
+         var $newThumbnail = buildCollectionItemTemplate();
+         $collectionContainer.append($newThumbnail);
      }
- }
+ });
